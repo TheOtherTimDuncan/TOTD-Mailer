7d07eebf5e9da30f9de3c935c1466ac9566a689f
Tim <tduncan72@gmail.com>
2015-11-02 18:48:59 -0500

    Change emailer to use handlebars to generate email content

--- run.js
+++ run.js
@@ -2,9 +2,11 @@
 
 var azure = require('azure-storage');
 var sendgrid = require('sendgrid')(process.env.SENDGRIDKEY);
-var app = require('lib/app.js');
+var handlebars = require('handlebars');
+
+var app = require('./lib/app.js');
 
 console.log('Using storage account ' + process.env.AZURE_STORAGE_ACCOUNT);
 
 var queueService = azure.createQueueService();
-app.sendEmailFromQueue(queueService, 'email', sendgrid);
+app.sendEmailFromQueue(queueService, 'email', sendgrid, handlebars);
